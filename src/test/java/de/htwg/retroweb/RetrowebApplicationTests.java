ea04badfce3a7ee76ec0d69393ae212d609b283c
Dr. F.-K. Koschnick <40572977+fkosch@users.noreply.github.com>
2022-03-03 11:22:19 +0100

    Update RetrowebApplicationTests.java

--- src/test/java/de/htwg/retroweb/RetrowebApplicationTests.java
+++ src/test/java/de/htwg/retroweb/RetrowebApplicationTests.java
@@ -3,7 +3,6 @@ package de.htwg.retroweb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class RetrowebApplicationTests {
 
