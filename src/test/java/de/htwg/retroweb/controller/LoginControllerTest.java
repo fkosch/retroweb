a845a75e43e9e2ceb16662436b6ec71a55d1f037
Dr. F.-K. Koschnick <40572977+fkosch@users.noreply.github.com>
2022-03-22 15:23:41 +0100

    Update LoginControllerTest.java

--- src/test/java/de/htwg/retroweb/controller/LoginControllerTest.java
+++ src/test/java/de/htwg/retroweb/controller/LoginControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
