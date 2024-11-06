e40bf4f77e7e659b558979d0329217a924c6c05c
Dr. F.-K. Koschnick <fritz.koschnick@t-online.de>
2024-11-06 10:25:35 +0100

    X

--- src/main/java/de/htwg/retroweb/controller/AdminController.java
+++ src/main/java/de/htwg/retroweb/controller/AdminController.java
@@ -28,7 +28,7 @@ public class AdminController {
     
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String admin(Map<String, Object> model, HttpServletRequest request) {
-    	//test comment
+    	//test comment2
     	LOG.debug("--> admin");
     	HttpSession session = request.getSession();
     	
